5f155fb02a0d2b71da17180b0ccacf53d3e94eec
joe <as955133@gmail.com>
2021-11-26 22:03:45 +0800

    增加例外攔截

--- HTTP_Filter_Collection/HTTP_Filter_Collection/Middleware/HttpLogMiddleware.cs
+++ HTTP_Filter_Collection/HTTP_Filter_Collection/Middleware/HttpLogMiddleware.cs
@@ -17,46 +17,57 @@ namespace HTTP_Filter_Collection.Middleware
 
         public async Task Invoke(HttpContext context)
         {
+            var originalBodyStrem = context.Response.Body;
             try
             {
                 string requestContent = await FromRequest(context.Request);
-                Console.WriteLine($"Request.Body={requestContent}");
 
-                var originalBodyStrem = context.Response.Body;                           
-
-                using (var newResponseBody = new MemoryStream())
+                try
                 {
-                    context.Response.Body = newResponseBody;
+                    using (var newResponseBody = new MemoryStream())
+                    {
+                        context.Response.Body = newResponseBody;
 
-                    await _next(context);
+                        await _next(context);
+                        //throw new Exception("123");
+                        string responseContent = await FromResponse(newResponseBody, originalBodyStrem);
+                        //throw new Exception("123");
 
-                    string responseContent = await FromResponse(newResponseBody, originalBodyStrem);
-
-                    Console.WriteLine($"Response.Body={responseContent}");
-
-                    var Request = $"Request- Path: {context.Request.Path}, Method: {context.Request.Method}, QueryString: {context.Request.QueryString},  Body: {requestContent}";
-                    var Response = $"Response - {context.Response.StatusCode}, Body: {responseContent}";
+                        var Request = $"Request- Path: {context.Request.Path}, Method: {context.Request.Method}, QueryString: {context.Request.QueryString},  Body: {requestContent}";
+                        var Response = $"Response - {context.Response.StatusCode}, Body: {responseContent}";
+                    }
                 }
-
-               
-
+                catch (Exception ex)
+                {
+                    //例外測試 當響應開始時無法修改 ContentType 跟 StatusCode
+                    context.Response.Body = new MemoryStream();
+                    //context.Response.ContentType = "application/json";
+                    //context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    await context.Response.WriteAsync($"{context.Response.StatusCode} : {ex}");
+                }
             }
-            catch (Exception ec)
+            catch (Exception ex)
             {
-
-                throw;
+                await ExceptionAsync(context, ex);
             }
-         
+
         }
+
+        private static Task ExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            return context.Response.WriteAsync($"{context.Response.StatusCode} : {exception}");
+        }
+
 
         private async Task<string> FromResponse(MemoryStream newBodyStrem, Stream originalBodyStrem)
         {
             string responseContent;
             newBodyStrem.Seek(0, SeekOrigin.Begin);
-            using (var reader = new StreamReader(newBodyStrem, encoding: Encoding.UTF8,
-                                              detectEncodingFromByteOrderMarks: false,
-                                              bufferSize: 1024,
-                                              leaveOpen: true))
+            using (var reader = new StreamReader(newBodyStrem,
+                encoding: Encoding.UTF8, detectEncodingFromByteOrderMarks: false, bufferSize: 1024, leaveOpen: true))
             {
                 responseContent = await reader.ReadToEndAsync();
                 newBodyStrem.Seek(0, SeekOrigin.Begin);
@@ -71,14 +82,10 @@ namespace HTTP_Filter_Collection.Middleware
         {
             string requestContent;
             request.EnableBuffering();
-            using (var reader = new StreamReader(request.Body, 
-                                              encoding: Encoding.UTF8,
-                                              detectEncodingFromByteOrderMarks: false,
-                                              bufferSize: 1024,
-                                              leaveOpen: true))
+            using (var reader = new StreamReader(request.Body,
+                 encoding: Encoding.UTF8, detectEncodingFromByteOrderMarks: false, bufferSize: 1024, leaveOpen: true))
             {
                 requestContent = await reader.ReadToEndAsync();
-           
                 request.Body.Seek(0, SeekOrigin.Begin);
             }
 
